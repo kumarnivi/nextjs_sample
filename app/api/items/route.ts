9c14428c80ee0ce9c7fc5bcdf848ac02e9fecd17
kumarnivi <nivethikashivakumar56@gmail.com>
2025-06-16 22:26:20 +0530

    post request Creation

--- app/api/items/route.ts
+++ app/api/items/route.ts
@@ -1,4 +1,6 @@
-export async function GET(){
+export async function GET(req:any){
+    const type = req.nextUrl.searchParams('type')
+    console.log('type:', type)
  return Response.json([
     {
         title:'I phone 12 pro'
