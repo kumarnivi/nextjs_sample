8c8ff94af87a8a4120807983ec3748e18e2574de
kumarnivi <nivethikashivakumar56@gmail.com>
2025-06-16 22:14:50 +0530

    APi Creation

--- app/api/items/route.ts
+++ app/api/items/route.ts
@@ -1,3 +1,15 @@
-// export default   async  () => {
+export async function GET(){
+ return Response.json([
+    {
+        title:'I phone 12 pro'
+    },
+    {
+        title:'Samsung'
+    },
 
-// }
+ ])
+}
+
+export async function POST(){
+    return Response.json({message:'post succesed'})
+}
